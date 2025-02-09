1c493ddede04502e8da3436854e27dcc09d26d4a
Luca Ingold <luca.ingold@gmail.com>
2025-02-09 18:44:56 +0100

    fix

---
+++ bin/DEV_Config.d
@@ -0,0 +1,2 @@
+./bin/DEV_Config.o: lib/Config/DEV_Config.c lib/Config/DEV_Config.h \
+./bin/  lib/Config/Debug.h
--- lib/Fonts/fonts.h
+++ lib/Fonts/fonts.h
@@ -1,1 +1,17 @@
+#ifndef __FONTS_H
+#define __FONTS_H
+
+#include <stdint.h>
+
+// Font structure definition
+typedef struct {
+    const uint8_t *table;
+    uint16_t width;
+    uint16_t height;
+} sFONT;
+
+// Font declarations
+extern sFONT Font12;
+
+#endif /* __FONTS_H */
 
